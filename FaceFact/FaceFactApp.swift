6c005530ca30dd2ab9a761e7e84da1b1d38f76e8
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-07 12:21:36 +0530

    Initial Commit

---
+++ FaceFact/FaceFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactApp.swift
+//  FaceFact
+//
+//  Created by Mukthar Amiyan on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
